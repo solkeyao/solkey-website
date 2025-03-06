6feae16ea1d1535ea4e51aa6991c5768df9643c3
Sebastião Sukuakueche <sukuakueches@gmail.com>
2025-03-06 18:14:51 +0100

    add: more parterns and brands
    Signed-off-by: Sebastião Sukuakueche <sukuakueches@gmail.com>

--- src/components/parceiros/index.jsx
+++ src/components/parceiros/index.jsx
@@ -3,10 +3,24 @@ import './index.css';
 export default function Parcerios(){
     return (
         <section className='d-section' id='parceiros'>
-            <h1 className='text-center'>Parceiros</h1>
+            <h1 className='text-center'>Parceiros & Marcas</h1>
             <ul className='parc_list'>
                 <li className='parc_item'>
                     <img  width='120px' src="/assets/parc_conta_it.png" alt="" />
+                </li>
+                <li className='parc_item'>
+                    <img  width='70px' src="/assets/tarcas.png" alt="" />
+                </li>
+
+                <li className='parc_item'>
+                    <img  width='70px' src="/assets/keypay.png" alt="" />
+                </li>
+                <li className='parc_item'>
+                    <img  width='80px' src="/assets/interexpress.png" alt="" />
+                </li>
+
+                <li className='parc_item'>
+                    <img  width='100px' src="/assets/voice_angola.png" alt="" />
                 </li>
             </ul>
         </section>
